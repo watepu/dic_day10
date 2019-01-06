a7d94ef73118163aa96b046a47a7f51424598ef1
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2019-01-06 20:12:06 +0900

    create blog function

---
+++ db/migrate/20190106105520_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :blogs do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
