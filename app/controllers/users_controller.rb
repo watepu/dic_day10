578c8499639af663be4a62b99eba0a2d8cb9cbb2
watepu <40508321+watepu@users.noreply.github.com>
2019-01-06 20:42:41 +0900

    user登録機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to new_session_path
+    else
+      render 'new'
+    end
   end
 
   def show
